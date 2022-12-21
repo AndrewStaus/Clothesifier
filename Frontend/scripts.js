9f14f6a4398eef256723096e14e8d1d2fb9086d7
Andrew Staus <adstaus@hotmail.com>
2022-12-20 21:36:29 -0700

    modified:   Frontend/scripts.js 	modified:   index.html

--- Frontend/scripts.js
+++ Frontend/scripts.js
@@ -29,19 +29,8 @@ for(property in object){
 
 async function uploadFile() {
   var $alert = $('.alert');
-  let formData = new FormData();
-
-  const canvas = document.createElement('canvas');
-  const ctx = canvas.getContext("2d");
-
-  canvas.width = 28;
-  canvas.height = 28;
-
-  ctx.drawImage(fileSelect.files[0], 0, 0, canvas.width, canvas.height);
-  const imageBlob = canvas.toBlob();
-  console.log(imageBlob.width)
-
-  formData.append("file", imageBlob);
+  let formData = new FormData();           
+  formData.append("file", fileSelect.files[0]);
   $.ajax('https://xxlkbgor75nvr7qw256z2xnrdm0ppqai.lambda-url.us-east-2.on.aws/image', {
       method: 'POST',
       data: formData,
@@ -58,74 +47,3 @@ async function uploadFile() {
           }
   });
 }
-
-
-
-window.uploadPhotos = function(){
-  // Read in file
-  var file = fileSelect.files[0];
-
-  // Ensure it's an image
-  if(file.type.match(/image.*/)) {
-      // Load the image
-      var reader = new FileReader();
-      reader.onload = function (readerEvent) {
-          var image = new Image();
-          image.onload = function (imageEvent) {
-
-              // Resize the image
-              var canvas = document.createElement('canvas'),
-                  max_size = 28,
-                  width = image.width,
-                  height = image.height;
-              if (width > height) {
-                  if (width > max_size) {
-                      height *= max_size / width;
-                      width = max_size;
-                  }
-              } else {
-                  if (height > max_size) {
-                      width *= max_size / height;
-                      height = max_size;
-                  }
-              }
-              canvas.width = width;
-              canvas.height = height;
-              canvas.getContext('2d').drawImage(image, 0, 0, width, height);
-              var dataUrl = canvas.toDataURL('image/jpeg');
-              $.event.trigger({
-                  type: "imageResized",
-                  url: dataUrl
-              });
-          }
-          image.src = readerEvent.target.result;
-      }
-      reader.readAsDataURL(file);
-  }
-};
-
-
-/* Handle image resized events */
-$(document).on("imageResized", function (event) {
-  var formData = new FormData($("form[id*='uploadImageForm']")[0]);
-  formData.append('image_data', event.dataUrl);
-
-    $.ajax({
-        url: 'https://xxlkbgor75nvr7qw256z2xnrdm0ppqai.lambda-url.us-east-2.on.aws/image',
-        data: formData,
-        cache: false,
-        contentType: false,
-        processData: false,
-        type: 'POST',
-
-        success: function (data) {
-          $alert.hide();
-          document.getElementById('result').innerHTML = data.category
-          updateTable(data.confs)
-        },
-        error: function () {
-          $alert.show();
-          }
-    });
-  
-});
