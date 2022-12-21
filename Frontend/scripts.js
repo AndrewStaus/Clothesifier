db0add23ece5baf0902c4ac1fcaaa7a8123ccb67
Andrew Staus <adstaus@hotmail.com>
2022-12-20 21:30:27 -0700

    modified:   Frontend/scripts.js

--- Frontend/scripts.js
+++ Frontend/scripts.js
@@ -93,10 +93,8 @@ window.uploadPhotos = function(){
               canvas.height = height;
               canvas.getContext('2d').drawImage(image, 0, 0, width, height);
               var dataUrl = canvas.toDataURL('image/jpeg');
-              var resizedImage = dataURLToBlob(dataUrl);
               $.event.trigger({
                   type: "imageResized",
-                  blob: resizedImage,
                   url: dataUrl
               });
           }
@@ -107,36 +105,10 @@ window.uploadPhotos = function(){
 };
 
 
-/* Utility function to convert a canvas to a BLOB */
-var dataURLToBlob = function(dataURL) {
-  var BASE64_MARKER = ';base64,';
-  if (dataURL.indexOf(BASE64_MARKER) == -1) {
-      var parts = dataURL.split(',');
-      var contentType = parts[0].split(':')[1];
-      var raw = parts[1];
-
-      return new Blob([raw], {type: contentType});
-  }
-
-  var parts = dataURL.split(BASE64_MARKER);
-  var contentType = parts[0].split(':')[1];
-  var raw = window.atob(parts[1]);
-  var rawLength = raw.length;
-
-  var uInt8Array = new Uint8Array(rawLength);
-
-  for (var i = 0; i < rawLength; ++i) {
-      uInt8Array[i] = raw.charCodeAt(i);
-  }
-
-  return new Blob([uInt8Array], {type: contentType});
-}
-/* End Utility function to convert a canvas to a BLOB      */
-
 /* Handle image resized events */
 $(document).on("imageResized", function (event) {
   var formData = new FormData($("form[id*='uploadImageForm']")[0]);
-  formData.append('image_data', event.blob);
+  formData.append('image_data', event.dataUrl);
 
     $.ajax({
         url: 'https://xxlkbgor75nvr7qw256z2xnrdm0ppqai.lambda-url.us-east-2.on.aws/image',
