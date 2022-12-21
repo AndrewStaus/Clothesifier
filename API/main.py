0554f70227d8a3165ab92815cdb48a951dcb2133
Andrew Staus <adstaus@hotmail.com>
2022-12-20 23:33:46 -0700

    renamed:    Backend/main.py -> API/main.py 	renamed:    Backend/model.tflite -> API/model.tflite 	renamed:    Backend/requirements.txt -> API/requirements.txt 	modified:   index.html 	renamed:    Frontend/scripts.js -> webpage/scripts.js 	renamed:    Frontend/shirt.svg -> webpage/shirt.svg 	renamed:    Frontend/styles.css -> webpage/styles.css

--- API/main.py
+++ API/main.py
@@ -1,5 +1,5 @@
 from tflite_runtime.interpreter import Interpreter
-from fastapi import FastAPI, Form, UploadFile, HTTPException
+from fastapi import FastAPI, Form, HTTPException
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
@@ -11,14 +11,12 @@ import numpy as np
 import base64
 import io
 
+INTR = Interpreter('model.tflite')
 LABELS = {0: 'T-shirt/top', 1: 'Trouser', 2: 'Pullover', 3: 'Dress', 4: 'Coat',
           5: 'Sandal',      6: 'Shirt',   7: 'Sneaker',  8: 'Bag',   9: 'Ankle boot'}
 
 app = FastAPI()
-
-
 handler = Mangum(app)
-INTR = Interpreter('model.tflite')
 
 def set_input_tensor(interpreter, image):
     interpreter.allocate_tensors()
@@ -48,13 +46,13 @@ def process(content):
         square.paste(image, ((height - width) // 2, 0))
     image = square
 
-    
     image = image.resize((28,28)) # resize to 28 by 28
     image = image.convert('L') # convert to black and white
     image = invert(image) # invert color
 
     image = np.array(image) # convert to numpy array
     image = image / 255 # normalize values to be between 0 and 1
+    image = (image-np.min(image)) / (np.max(image)-np.min(image))
     image = np.reshape(image, (28, 28, 1)) # reshape
 
     return image
@@ -88,4 +86,8 @@ async def get_image(filename: str = Form(...), filedata: str = Form(...)):
         
 @app.get("/")
 def root():
-    return {'message':['Welcome to Clothesifier API', 'Git Page: https://github.com/AndrewStaus/Clothesifier', 'Website:']}
+    return {'message':[
+        'Welcome to Clothesifier API',
+        'Git Page: https://github.com/AndrewStaus/Clothesifier',
+        'Website: https://andrewstaus.github.io/Clothesifier/'
+        ]}
